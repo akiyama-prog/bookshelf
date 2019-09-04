821cb479bdda1757937fa739639ceaa2014573a8
yamada <autumn_ioy@yahoo.co.jp>
2019-09-04 11:30:41 +0000

    navtab booklist

--- resources/views/users/navtab.blade.php
+++ resources/views/users/navtab.blade.php
@@ -1,6 +1,6 @@
 <ul class="nav nav-tabs nav-justified mb-3">
     <li class="nav-item"><a href="{{ route('users.show', ['id' => $user->id]) }}" class="text-muted nav-link {{ Request::is('users/' . $user->id) ? 'active' : '' }}">my bookshelf <span class="badge badge-secondary">{{ $count_book_records }}</span></a></li>
-    <li class="nav-item"><a href="{{ route('users.book_lists',['id' => $user->id]) }}" class="text-muted nav-link {{ Request::is('users/*/book_lists') ? 'active' : '' }}">to read book list <span class ="badge badge-secondary">{{ $count_book_lists }}</span></a></li>
+    <li class="nav-item"><a href="{{ route('users.book_lists',['id' => $user->id]) }}" class="text-muted nav-link {{ Request::is('users/*/book_lists') ? 'active' : '' }}">book list <span class ="badge badge-secondary">{{ $count_book_lists }}</span></a></li>
     @if(Auth::id() == $user->id)
     <li class="nav-item"><a href="{{ route('book_records.create',['id' => $user->id]) }}" class="text-muted nav-link {{ Request::is('book_records/create') ? 'active' : '' }}">add new book</a></li>
     @endif
